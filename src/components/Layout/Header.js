8ad71d2b504281c5a498700b6d0ea7feb5a4414f
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-05-30 16:00:24 +0530

    Making the Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react"
+import classes from "./Header.module.css"
+import mealImgae from '../../assests/meals.jpg'
+
+const Header = (props) => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealImgae}  alt="A Table full of Meals"/>
+        </div>
+    </Fragment>
+
+}
+
+export default Header
